f369b5ea375131fcbd4d0ac9b543faa576e4149c
championway <cpwearth.eed03@g2.nctu.edu.tw>
2019-04-21 19:31:42 +0800

    update readme and gif

--- environment.sh
+++ environment.sh
@@ -4,15 +4,11 @@
 # XXX: not sure if this is the place to put this.
 export PYTHONPATH=$(pwd - P)/catkin_ws/src/utm
 export ROS_LANG_DISABLE=gennodejs:geneus:genlisp
-GAZEBO_MODEL_PATH=$(pwd - P)/catkin_ws/src/gazebo/model
 shell=`basename $SHELL`
 echo "Activating ROS with shell: $SHELL"
 source /opt/ros/kinetic/setup.$shell
-export GAZEBO_MODEL_PATH
 export HOSTNAME=$HOSTNAME
 export ROS_HOSTNAME=$HOSTNAME.local
-echo "Set ROS_HOSTNAME to: $ROS_HOSTNAME"
-echo "Set GAZEBO_MODEL_PATH to: $GAZEBO_MODEL_PATH"
 if [ 2015 -ge $(date +%Y) ];
 then
     >&2 echo "Error! Time travel detected. System time is: $(date)"
