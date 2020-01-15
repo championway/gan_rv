2bdb6f92aa5c5c52fdda86901504d4f5701af8bf
championway <cpwearth.eed03@g2.nctu.edu.tw>
2020-01-16 03:24:08 +0800

    update

--- catkin_ws/src/pix2pix/datasets_pst_no_crop.py
+++ catkin_ws/src/pix2pix/datasets_pst_no_crop.py
@@ -18,6 +18,7 @@ class ImageDataset(Dataset):
     def __init__(self, root = "/media/arg_ws3/5E703E3A703E18EB/data/PST900_RGBT_Dataset/", transforms_A=None, transforms_B=None, mode='train'):
         self.transform_A = transforms.Compose(transforms_A)
         self.transform_B = transforms.Compose(transforms_B)
+        self.mode = mode
         self.root = root
         self.border = 0
         self.rnd_border = 20
@@ -36,7 +37,7 @@ class ImageDataset(Dataset):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         crop_list = []
 
-        if np.random.random() < 0.5:
+        if np.random.random() < 0.5 and self.mode == 'train':
             image = np.array(image)[:-1:, :]
             mask = np.array(mask)[:-1:, :]
 
@@ -47,17 +48,17 @@ class ImageDataset(Dataset):
         n_class = 5
         h, w = mask.shape
         target = torch.zeros(n_class, h, w)
-        mask[mask!=0] = 1 # 255 to 1
-        mask = torch.from_numpy(mask.copy()).long()
+        # mask[mask!=0] = 1 # 255 to 1
+        mask_ = torch.from_numpy(mask.copy()).long()
         for i in range(n_class):
-            target[i][mask == i] = 1
+            target[i][mask_ == i] = 1
 
         #target = target.unsqueeze(dim=0)
         # img_depth = torch.tensor(np.array(img_depth)).unsqueeze(dim=0)
         image = Image.fromarray(image).resize((256, 256), Image.ANTIALIAS)
         image = self.transform_A(image)
 
-        return {'A': target, 'B': image}
+        return {'A': target, 'B': image, 'l':mask}
 
     def __len__(self):
         return len(self.img_list)
--- catkin_ws/src/pix2pix/pix2pix_pst_no_crop.py
+++ catkin_ws/src/pix2pix/pix2pix_pst_no_crop.py
@@ -13,6 +13,7 @@ from torchvision.utils import save_image
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torch.autograd import Variable
+from sklearn.metrics import confusion_matrix
 
 from models_subt import *
 from datasets_pst_no_crop import *
@@ -24,7 +25,7 @@ import torch
 parser = argparse.ArgumentParser()
 parser.add_argument('--epoch', type=int, default=0, help='epoch to start training from')
 parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
-parser.add_argument('--dataset_name', type=str, default="pst_pix_no_crop", help='name of the dataset')
+parser.add_argument('--dataset_name', type=str, default="unet", help='name of the dataset')
 parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
 parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
 parser.add_argument('--b1', type=float, default=0.5, help='adam: decay of first order momentum of gradient')
@@ -38,9 +39,10 @@ parser.add_argument('--sample_interval', type=int, default=200, help='interval b
 parser.add_argument('--checkpoint_interval', type=int, default=5, help='interval between model checkpoints')
 opt = parser.parse_args()
 print(opt)
-root = '/media/arg_ws3/5E703E3A703E18EB/research/pix2pix_cropmask/'
+root = '/media/arg_ws3/5E703E3A703E18EB/research/subt_fcn/'
 os.makedirs(root + 'images/%s' % opt.dataset_name, exist_ok=True)
 os.makedirs(root + 'saved_models/%s' % opt.dataset_name, exist_ok=True)
+score_dir = os.path.join("/media/arg_ws3/5E703E3A703E18EB/research/subt_fcn/scores", opt.dataset_name)
 
 cuda = True if torch.cuda.is_available() else False
 
@@ -56,6 +58,7 @@ patch = (1, opt.img_height//2**4, opt.img_width//2**4)
 
 # Initialize generator and discriminator
 out_channels = 5
+n_class = out_channels
 generator = GeneratorUNet(in_channels=3, out_channels=out_channels)
 discriminator = Discriminator(in_channels=out_channels+3)
 
@@ -93,10 +96,92 @@ dataloader = DataLoader(ImageDataset(transforms_A=transforms_B, transforms_B=tra
                         batch_size=opt.batch_size, shuffle=True, num_workers=opt.n_cpu)
 
 val_dataloader = DataLoader(ImageDataset(transforms_A=transforms_B, transforms_B=transforms_B, mode='test'),
-                            batch_size=10, shuffle=True, num_workers=1)
+                            batch_size=1, shuffle=True, num_workers=1)
 
 # Tensor type
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+
+def val(epoch):
+    generator.eval()
+    TP = np.zeros(n_class-1, dtype = np.float128)
+    FN = np.zeros(n_class-1, dtype = np.float128)
+    FP = np.zeros(n_class-1, dtype = np.float128)
+    total_ious = []
+    pixel_accs = []
+    for iter, batch in enumerate(val_dataloader):
+        inputs = Variable(batch['B'].type(Tensor))
+        output = generator(inputs)
+        output = output.data.cpu().numpy()
+
+        N, _, h, w = output.shape
+        # print("output: ", output.shape)
+        pred = output.transpose(0, 2, 3, 1).reshape(-1, n_class).argmax(axis=1).reshape(N, h, w)
+        # print("pred: ", pred.shape)
+        target = batch['l'].cpu().numpy().reshape(N, h, w)
+        for p, t in zip(pred, target):
+            pixel_accs.append(pixel_acc(p, t))
+            _TP, _FN, _FP =  analysis(p, t, h, w)
+            TP += _TP[1:n_class]
+            FN += _FN[1:n_class]
+            FP += _FP[1:n_class]
+            
+    recall = TP / (TP + FN)
+    precision = TP / (TP + FP)
+    ious = TP / (TP + FN + FP)
+    fscore = 2*TP / (2*TP + FN + FP)
+    total_ious = np.array(total_ious).T  # n_class * val_len
+    pixel_accs = np.array(pixel_accs).mean()
+    
+    #print("epoch{}, pix_acc: {}, meanIoU: {}, IoUs: {}, recall: {}, precision: {}, fscore: {}"\
+    #      .format(epoch, pixel_accs, np.nanmean(ious), ious, recall, precision, fscore))
+    print("epoch{}, meanIoU: {}".format(epoch, np.nanmean(ious)))
+    
+    f1 = open(score_dir + "/cls_acc_log.txt","a+")
+    f1.write('epoch:'+ str(epoch) + ', pix_acc: ' + str(pixel_accs) + '\n' )
+    f2 = open(score_dir + "/cls_iou_log.txt","a+")
+    f2.write('epoch:'+ str(epoch) + ', class ious: ' + str(ious) + '\n' )
+    f3 = open(score_dir + "/mean_iou_log.txt","a+")
+    f3.write('epoch:'+ str(epoch) + ', mean IoU: ' + str(np.nanmean(ious)) + '\n' ) 
+    f4 = open(score_dir + "/recall_log.txt","a+")
+    f4.write('epoch:'+ str(epoch) + ', class recall: ' + str(recall) + '\n' )
+    f5 = open(score_dir + "/precision_log.txt","a+")
+    f5.write('epoch:'+ str(epoch) + ', class precision: ' + str(precision) + '\n' )    
+    f6 = open(score_dir + "/fscore_log.txt","a+")
+    f6.write('epoch:'+ str(epoch) + ', class fscore: ' + str(fscore) + '\n' )  
+    f7 = open(score_dir + "/mean_fscore_log.txt","a+")
+    f7.write('epoch:'+ str(epoch) + ', mean fscore: ' + str(np.nanmean(fscore)) + '\n' )
+    f8 = open(score_dir + "/mean_precision_log.txt","a+")
+    f8.write('epoch:'+ str(epoch) + ', mean precision: ' + str(np.nanmean(precision)) + '\n' ) 
+    f9 = open(score_dir + "/mean_recall_log.txt","a+")
+    f9.write('epoch:'+ str(epoch) + ', mean recall: ' + str(np.nanmean(recall)) + '\n' ) 
+    
+
+def analysis(pred, target, h, w):
+    # TP, FN, FP, TN
+    TP = np.zeros(n_class, dtype = np.float128)
+    FN = np.zeros(n_class, dtype = np.float128)
+    FP = np.zeros(n_class, dtype = np.float128)
+
+    target = target.reshape(h * w)
+    pred = pred.reshape(h * w)
+
+    con_matrix = confusion_matrix(target, pred,labels = np.arange(0,n_class,1))
+    con_matrix[0][0] = 0
+    for i in range(0, n_class):
+        for j in range(0, n_class):
+            if i == j:
+                TP[i] += con_matrix[i][j]
+            if i != j:
+                FP[j] += con_matrix[i][j]
+                FN[i] += con_matrix[i][j]
+    return TP, FN, FP
+                
+def pixel_acc(pred, target):
+    correct = (pred == target).sum()
+    total   = (target == target).sum()
+    return correct / total
+
 
 def sample_images(batches_done):
     n_class = out_channels
@@ -107,11 +192,10 @@ def sample_images(batches_done):
     real_B = real_B.data.cpu().numpy()
     N, _, h, w = real_B.shape
     mask = real_B.transpose(0, 2, 3, 1).reshape(-1, n_class).argmax(axis = 1).reshape(N, h, w)
-    
     fake_B = generator(real_A)
     fake_B = fake_B.data.cpu().numpy()
     gen = fake_B.transpose(0, 2, 3, 1).reshape(-1, n_class).argmax(axis = 1).reshape(N, h, w)
-    print(gen[gen!=0])
+    # print(gen[gen!=0])
     fake_B_d = torch.tensor(gen).type(Tensor).unsqueeze(1).data
     fake_B_d = torch.cat((fake_B_d, fake_B_d, fake_B_d), 1)
     real_B_d = torch.tensor(mask).type(Tensor).unsqueeze(1).data
@@ -145,13 +229,14 @@ for epoch in range(opt.epoch, opt.n_epochs):
 
         # GAN loss
         fake_B = generator(real_A)
-        pred_fake = discriminator(fake_B, real_A)
-        loss_GAN = criterion_GAN(pred_fake, valid)
+        # pred_fake = discriminator(fake_B, real_A)
+        # loss_GAN = criterion_GAN(pred_fake, valid)
         # Pixel-wise loss
         loss_pixel = criterion_pixelwise(fake_B, real_B)
 
         # Total loss
-        loss_G = loss_GAN + lambda_pixel * loss_pixel
+        # loss_G = loss_GAN + lambda_pixel * loss_pixel
+        loss_G = loss_pixel
         #loss_G = loss_GAN
 
         loss_G.backward()
@@ -162,45 +247,50 @@ for epoch in range(opt.epoch, opt.n_epochs):
         #  Train Discriminator
         # ---------------------
 
-        optimizer_D.zero_grad()
+        # optimizer_D.zero_grad()
 
-        # Real loss
-        pred_real = discriminator(real_B, real_A)
-        loss_real = criterion_GAN(pred_real, valid)
+        # # Real loss
+        # pred_real = discriminator(real_B, real_A)
+        # loss_real = criterion_GAN(pred_real, valid)
 
-        # Fake loss
-        pred_fake = discriminator(fake_B.detach(), real_A)
-        loss_fake = criterion_GAN(pred_fake, fake)
+        # # Fake loss
+        # pred_fake = discriminator(fake_B.detach(), real_A)
+        # loss_fake = criterion_GAN(pred_fake, fake)
 
-        # Total loss
-        loss_D = 0.5 * (loss_real + loss_fake)
+        # # Total loss
+        # loss_D = 0.5 * (loss_real + loss_fake)
 
-        loss_D.backward()
-        optimizer_D.step()
+        # loss_D.backward()
+        # optimizer_D.step()
 
-        # --------------
-        #  Log Progress
-        # --------------
+        # # --------------
+        # #  Log Progress
+        # # --------------
 
-        # Determine approximate time left
+        # # Determine approximate time left
         batches_done = epoch * len(dataloader) + i
         batches_left = opt.n_epochs * len(dataloader) - batches_done
         time_left = datetime.timedelta(seconds=batches_left * (time.time() - prev_time))
         prev_time = time.time()
 
         # Print log
-        sys.stdout.write("\r[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f, pixel: %f, adv: %f] ETA: %s" %
+        # sys.stdout.write("\r[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f, pixel: %f, adv: %f] ETA: %s" %
+        #                                                 (epoch, opt.n_epochs,
+        #                                                 i, len(dataloader),
+        #                                                 loss_D.item(), loss_G.item(),
+        #                                                 loss_pixel.item(), loss_GAN.item(),
+        #                                                 time_left))
+        sys.stdout.write("\r[Epoch %d/%d] [Batch %d/%d] [G loss: %f] ETA: %s" %
                                                         (epoch, opt.n_epochs,
                                                         i, len(dataloader),
-                                                        loss_D.item(), loss_G.item(),
-                                                        loss_pixel.item(), loss_GAN.item(),
+                                                        loss_G.item(),
                                                         time_left))
 
         # If at sample interval save image
-        if batches_done % opt.sample_interval == 0:
-            sample_images(batches_done)
-
-
+        # if batches_done % opt.sample_interval == 0:
+            # sample_images(batches_done)
+    val(epoch)
+    sample_images(epoch)
     if opt.checkpoint_interval != -1 and epoch % opt.checkpoint_interval == 0:
         # Save model checkpoints
         torch.save(generator.state_dict(), root + 'saved_models/%s/generator_%d.pth' % (opt.dataset_name, epoch))
